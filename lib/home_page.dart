3041fadacf161d5586e704b336b157ea520a47b6
Sadid <sadid.jones@gmail.com>
2023-05-07 22:51:05 +0600

    5/7/2023

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:bloc_rohit/blocs/internet_bloc/internet_bloc.dart';
+import 'package:bloc_rohit/blocs/internet_bloc/internet_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -9,9 +12,18 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: const SafeArea(
-          child: Center(
-        child: Text('Loading.......'),
+      body: SafeArea(child: Center(
+        child: BlocBuilder<InternetBloc, InternetState>(
+          builder: (context, state) {
+            if (state is InternetGainedState) {
+              return const Text('Connected');
+            } else if (state is InternetLostState) {
+              return const Text("Not Connected");
+            } else {
+              return const Text('Loading');
+            }
+          },
+        ),
       )),
     );
     ;
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:bloc_rohit/blocs/internet_bloc/internet_bloc.dart';
 import 'package:bloc_rohit/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +13,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const HomePage());
+    return BlocProvider(
+      create: (context) => InternetBloc(),
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const HomePage()),
+    );
   }
 }
