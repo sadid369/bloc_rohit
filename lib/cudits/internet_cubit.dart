227e9f0c94c422a750e83c36eb41d76fe3e3295d
Sadid <sadid.jones@gmail.com>
2023-05-12 14:56:31 +0600

    cubit finished

---
+++ lib/cudits/internet_cubit.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+enum InternetState { Initial, Lost, Gained, GainedMobile, GainedWifi }
+
+class InternetCubit extends Cubit<InternetState> {
+  Connectivity _connectivity = Connectivity();
+  StreamSubscription? connectivitySubscription;
+  InternetCubit() : super(InternetState.Initial) {
+    connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen((result) {
+      if (result == ConnectivityResult.mobile ||
+          result == ConnectivityResult.wifi) {
+        if (result == ConnectivityResult.mobile) {
+          emit(InternetState.GainedMobile);
+        } else if (result == ConnectivityResult.wifi) {
+          emit(InternetState.GainedWifi);
+        }
+      } else {
+        emit(InternetState.Lost);
+      }
+    });
+  }
+
+  @override
+  Future<void> close() {
+    connectivitySubscription!.cancel();
+    return super.close();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bloc_rohit/blocs/internet_bloc/internet_bloc.dart';
+import 'package:bloc_rohit/cudits/internet_cubit.dart';
 import 'package:bloc_rohit/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => InternetBloc(),
+      create: (context) => InternetCubit(),
       child: MaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'Flutter Demo',
